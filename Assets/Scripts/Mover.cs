a811615d5580a09c9269fa19d691d79afbffdc6e
Ben <brobbins2018@gmail.com>
2019-08-30 16:16:57 -0400

    Finished Ray Traceing

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,6 +12,12 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
